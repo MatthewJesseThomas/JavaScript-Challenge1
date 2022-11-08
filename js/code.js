4d2e1cd677983efc3307869b03252bcb15493474
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-11-08 10:35:05 +0200

    main

---
+++ js/code.js
@@ -0,0 +1,15 @@
+// let numb1 = prompt("Please Enter numb1");
+//5
+// let numb2 = prompt("Please Enter numb2");
+// let addition = numb1 + numb2;
+// console.log(addition);
+// let numb1 = 4;
+// let numb2 = 3;
+// let product = eval (`${numb1}*${numb2}`);
+// console.log(product);
+
+let userName = prompt("Provide UserName");
+let userSurname = prompt("Provide UserSurname");
+let userSalary = prompt("Provide UserSalary");
+let salary = eval(`${userSalary} + ${500}`);
+document.write(`Name: ${userName}<br> Surname: ${userSurname}<br>Salary: ${salary}<br>`);
